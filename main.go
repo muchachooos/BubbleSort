119b518385ae3bab7e2b0098087b15d60c57cbf0
muchachooos <odnorogg@bk.ru>
2022-10-18 09:41:18 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	slice := []int{74, 12, 32, 35, 96, 87, 27, 8, 54}
+
+	bubbleSort(slice)
 }
 
-func sort(slice []int) {
-	
+func bubbleSort(slice []int) {
+
+	//swap := func(a, b int) {
+	//
+	//}
+	//
+	//newSlice := make([]int, len(slice))
+
+	for i := range slice {
+		if slice[i] > slice[1] {
+			slice[i], slice[1] = slice[1], slice[i]
+		}
+	}
+
+	fmt.Println(slice)
 }
