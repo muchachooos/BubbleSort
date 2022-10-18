86fc6c6400a20dd65f0002fcbcb76dfe5ce73db2
muchachooos <odnorogg@bk.ru>
2022-10-18 17:56:09 +0400

    Update main.go

--- main.go
+++ main.go
@@ -4,37 +4,47 @@ import "fmt"
 
 func main() {
 	slice := []int{74, 12, 32, 35, 96, 87, 27, 8, 54, 6}
-
-	bubbleSort(slice)
-
+	fmt.Println(bubbleSort(slice))
 }
 
 func bubbleSort(slice []int) []int {
-
 	//var j int
 	//newSlice := make([]int, len(slice))
 
+	//var iKeeper int
+
 	for i := range slice {
+		if i == len(slice)-1 {
+			break
+		}
 		if slice[i] > slice[i+1] {
-			swap(slice[i], slice[i+1])
+			slice[i], slice[i+1] = slice[i+1], slice[i]
 		} else {
 			i++
 		}
 	}
+	return slice
 }
 
-func swap(a, b int) {
-
-	fmt.Println("a = ", a)
-	fmt.Println("b = ", b)
-	fmt.Println("--------")
-	//var bKeeper int
-
-	var aKeeper int
-	aKeeper = a
-	a = b
-	b = aKeeper
-
-	fmt.Println("a = ", a)
-	fmt.Println("b = ", b)
-}
+//if slice[i] > slice[i+1] {
+//slice[i] = iKeeper
+//slice[i] = slice[i+1]
+//slice[i+1] = iKeeper
+//}
+//var aKeeper int
+//aKeeper = a
+//a = b
+//b = aKeeper
+//--------------------------------
+//for i := range slice {
+//	if slice[i] > slice[i+1] {
+//		swap(slice[i], slice[i+1])
+//		i++
+//	} else {
+//		i++
+//	}
+//	if i == len(slice)-1 {
+//		break
+//	}
+//}
+//return slice
